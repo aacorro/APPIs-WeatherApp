568877fa81cba2da47c36fd764a92a51401c9e0d
Abdiel Corro <acorrop@outlook.com>
2022-11-28 13:50:41 -0600

    exercise completed

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+
+Console.WriteLine("Please enter your API key: ");
+
+var api_key = Console.ReadLine();
+
+while(true)
+{
+    Console.WriteLine();
+    Console.WriteLine("Please enter the city name: ");
+    var city_name = Console.ReadLine();
+
+    var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial";
+
+    var response = client.GetStringAsync(weatherURL).Result;
+
+    var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+
+    Console.WriteLine(formattedResponse);
+
+    Console.WriteLine();
+
+    Console.WriteLine("Would you like to choose a different city?");
+    var userInput = Console.ReadLine();
+
+    if(userInput.ToLower() == "no")
+    {
+        break;
+    };
+}
+
